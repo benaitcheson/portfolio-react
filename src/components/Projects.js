d7dd8370abe1972d5dc1e20eb3f4ad9bd5037cfd
benaitcheson <benaitcheson@hotmail.com>
2021-11-15 11:31:44 +1000

    testing text for grid on projects and deployment

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,7 +10,7 @@ const Projects = () => {
                 <div class="col-sm-6">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">Special title treatment</h5>
+                            <h5 class="card-title">Test change of text for deployment</h5>
                             <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
