a22d7b32e30b2a0eaac01e0b44691c03c39c6c3f
benaitcheson <benaitcheson@hotmail.com>
2021-11-16 09:55:07 +1000

    adding footer bootstrap styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <p>insert socials. Github. Linkedin.</p>
-            <p>copyright</p>
+        <footer class="footer mt-auto py-3 bg-light">
+            <div class="container">
+                <span class="text-muted">Place sticky footer content here.</span>
+            </div>
         </footer>
     )
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -3,10 +3,12 @@ import React from 'react';
 
 const MainContent = () => {
     return (
-        <main  class="mx-auto p-4 container">
-            <h1>Hi, my name is Ben Aitcheson.</h1>
-            <h1>Welcome to my portfolio page, I'll showcase my projects here.</h1>
-            <button>Find out more!</button>
+        <main  class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+                <h1 class="display-5 fw-bold">Hi, my name is Ben Aitcheson.</h1>
+                <h1 class="display-5 fw-bold">Welcome to my portfolio page, I'll showcase my projects here.</h1>
+                <button class="btn btn-primary btn-lg" type="button">Find out more!</button>
+            </div>
         </main>
     )
 }
