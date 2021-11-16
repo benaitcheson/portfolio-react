c754d8766cd99143a75688b2456fa332b60f3e48
benaitcheson <benaitcheson@hotmail.com>
2021-11-16 15:26:52 +1000

    added footer colour and horizontal rule

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,15 +3,20 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div class="container-fluid">
-            <div class="py-3 my-4">
-                <div class="col">
-                    <a href="https://codepen.io/benaitcheson"><i class="fab fa-codepen fa-2x"></i></a>
-                    <a href="https://github.com/benaitcheson"><i class="fab fa-github fa-2x"></i></a>
-                    <a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/"><i class="fab fa-linkedin fa-2x"></i></a>
+        <footer className="footer">
+            <div class="container">
+                <div class="py-3 my-4">
+                    <div class="col">
+                        <a href="https://codepen.io/benaitcheson"><i class="fab fa-codepen fa-2x"></i></a>
+                        <a href="https://github.com/benaitcheson"><i class="fab fa-github fa-2x"></i></a>
+                        <a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/"><i class="fab fa-linkedin fa-2x"></i></a>
+                    </div>
+
+                    <hr></hr>
+                    <p>copyright stuff</p>
                 </div>
             </div>
-        </div>
+        </footer>
     )
 }
 
