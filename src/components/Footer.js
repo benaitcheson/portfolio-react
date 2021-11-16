8f681fd096dd6ac456c68ef02f57ea7267cea8e3
benaitcheson <benaitcheson@hotmail.com>
2021-11-16 15:16:33 +1000

    playing around with the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div class="container">
-            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <ul class="nav col-md-4 justify-content-end">
-                    <li class="nav-item"><a href="https://github.com/benaitcheson" class="nav-link px-2 text-muted">Github</a></li>
-                    <li class="nav-item"><a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/" class="nav-link px-2 text-muted">LinkedIn</a></li>
-                    <li class="nav-item"><a href="https://codepen.io/benaitcheson" class="nav-link px-2 text-muted">Codepen</a></li>
-                </ul>
-            </footer>
+        <div class="container-fluid">
+            <div class="py-3 my-4">
+                <div class="col">
+                    <a href="https://codepen.io/benaitcheson"><i class="fab fa-codepen fa-2x"></i></a>
+                    <a href="https://github.com/benaitcheson"><i class="fab fa-github fa-2x"></i></a>
+                    <a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/"><i class="fab fa-linkedin fa-2x"></i></a>
+                </div>
+            </div>
         </div>
     )
 }
