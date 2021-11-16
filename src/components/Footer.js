7bb2d42e35a60e79ef9e055a41ba676666392ebe
benaitcheson <benaitcheson@hotmail.com>
2021-11-16 11:36:21 +1000

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer class="footer mt-auto py-3 bg-light">
-            <div class="container">
-                <span class="text-muted">Place sticky footer content here.</span>
-            </div>
-        </footer>
+        <div class="container">
+            <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <ul class="nav col-md-4 justify-content-end">
+                    <li class="nav-item"><a href="https://github.com/benaitcheson" class="nav-link px-2 text-muted">Github</a></li>
+                    <li class="nav-item"><a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/" class="nav-link px-2 text-muted">LinkedIn</a></li>
+                    <li class="nav-item"><a href="https://codepen.io/benaitcheson" class="nav-link px-2 text-muted">Codepen</a></li>
+                </ul>
+            </footer>
+        </div>
     )
 }
 
