1d8f9bfe3df944b33ff2a0fcbc9c5ca1edb71d12
benaitcheson <benaitcheson@hotmail.com>
2021-11-18 14:11:47 +1000

    adding flexbox to the logos in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,23 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="container">
-                <div className="py-3 my-4">
-                    <div className="col">
-                        <a href="https://codepen.io/benaitcheson"><i className="fab fa-codepen fa-2x"></i></a>
-                        <a href="https://github.com/benaitcheson"><i className="fab fa-github fa-2x"></i></a>
-                        <a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/"><i className="fab fa-linkedin fa-2x"></i></a>
+            <div className="">
+                <div className="container">
+                    <div className="py-5" id="footerContents">
+                        <ul className="row">
+                            <li className="col">
+                                <a href="https://codepen.io/benaitcheson"><i className="fab fa-codepen fa-2x"></i></a>                                
+                            </li>
+                            <li className="col">
+                                <a href="https://github.com/benaitcheson"><i className="fab fa-github fa-2x"></i></a>
+                            </li>
+                            <li className="col">
+                                <a href="https://www.linkedin.com/in/ben-aitcheson-75963b84/"><i className="fab fa-linkedin fa-2x"></i></a>
+                            </li>
+                        </ul>
                     </div>
 
-                    <hr></hr>
+                    <hr className="white"></hr>
                     <p>copyright stuff</p>
                 </div>
             </div>
